8c21784ed5cd94320086beb9163f5a84f293041a
Tobias Heinz <tobias.heinz@logmein.com>
2018-05-04 23:09:50 +0200

    Add functionality to start, pause and stop a game by keycodes

--- src/game.py
+++ src/game.py
@@ -5,11 +5,12 @@ from music import Music
 from window import Window
 
 import cv2
-import time
+
 
 class Game:
     def __init__(self):
         self.running = False
+        self.isPaused = False
         self.detector = Detector()
         self.frameSource = FrameSource()
         self.music = Music()
@@ -27,12 +28,25 @@ class Game:
         self.numKeypoints = 0
 
     def run(self):
-        self.numKeypoints = len(self.getStartKeypoints())
-        self.running = True
-        self.music.play(self.soundtrackFile, self.duration)
+        self.start()
 
         while self.running:
-            if self.music.checkDuration():
+            key = cv2.waitKey(50)
+            if key == ord('q'):
+                break
+            elif key == ord(' ') and not self.isPaused:
+                self.finish(Finishsounds.success)
+            elif key == ord('s'):
+                self.start()
+            elif key == ord('p'):
+                self.togglepause()
+            elif key == ord('e'):
+                self.stop()
+
+            if self.isPaused:
+                continue
+
+            if self.music.playtime_finished():
                 self.finish(Finishsounds.timeIsUp)
 
             check, frame = self.frameSource.grabFrame()
@@ -49,39 +63,52 @@ class Game:
             if len(self.currentKeypoints) != self.numKeypoints:
                 self.finish(Finishsounds.failure)
 
-            if self.previousKeypoints != None:
+            if self.previousKeypoints is not None:
                 self.update()
-            
-            key = cv2.waitKey(50)
-            if key == ord('q'):
-                break
 
-            key = cv2.waitKey(32)
-            if key == ord(' '):
-                self.finish(Finishsounds.success)
+    def start(self):
+        self.running = True
+        self.isPaused = False
+        self.numKeypoints = len(self.getkeypoints())
+        self.music.play(self.soundtrackFile, self.duration)
+        print('New game started')
+
+    def togglepause(self):
+        self.music.togglepause()
+        if self.isPaused:
+            print('Game resumed')
+        else:
+            print('Game paused')
+        self.isPaused = not self.isPaused
+
+    def stop(self):
+        self.isPaused = True
+        self.music.stop()
+        print('Game stopped')
 
     def finish(self, result):
+        if not self.music.currentFile == self.soundtrackFile:
+            return
         soundfile = self.finishSounds[result]
+        print('Game ended on ' + result.name + ' after ' + str(self.music.get_playtime()) + ' seconds ')
         self.music.play(soundfile)
-        time.sleep(3)
-        self.running = False
-
-
-    def getStartKeypoints(self):
-        check, frame = self.frameSource.grabFrame()
-        keypoints, frame_with_keypoints = self.detector.detect(frame)
-        return keypoints
 
     def update(self):
         pass
+
+    def getkeypoints(self):
+        check, frame = self.frameSource.grabFrame()
+        keypoints, frame_with_keypoints = self.detector.detect(frame)
+        return keypoints
     
     def destroy(self):
+        self.music.destroy()
         self.frameSource.destroy()
         self.window.destroy()
 
 if __name__ == '__main__':
-    print("Starting Game")
+    print('Starting Game')
     game = Game()
     game.run()
     game.destroy()
-    print("Game ended")
+    print('Game ended')
--- src/music.py
+++ src/music.py
@@ -1,27 +1,51 @@
 import pygame
 import time
 
+
 class Music:
     def __init__(self):
+        self.isPaused = False
         self.duration = 0
         self.soundPath = './sounds/'
+        self.currentFile = ''
         self.start = 0
         pygame.init()
 
     def play(self, file, duration=0):
         self.duration = duration
+
         if pygame.mixer.music.get_busy():
             self.stop()
+
+        self.currentFile = file
         pygame.mixer.music.load(self.soundPath+file)
         pygame.mixer.music.play(0)
         self.start = time.time()
 
+    def togglepause(self):
+        if pygame.mixer.music.get_busy():
+            if self.isPaused:
+                pygame.mixer.music.unpause()
+            else:
+                pygame.mixer.music.pause()
+            self.isPaused = not self.isPaused
+
     def stop(self):
         if pygame.mixer.music.get_busy():
+            self.currentFile = ''
             pygame.mixer.music.stop()
 
-    def checkDuration(self):
-        if self.duration > 0 and time.time() - self.start > self.duration:
-            self.stop()
-            return True
-        return False
+    def playtime_finished(self):
+        is_finished = self.duration > 0 and time.time() - self.start > self.duration
+        return is_finished
+
+    def get_playtime(self):
+        playtime = 0
+        if pygame.mixer.music.get_busy():
+            playtime = time.time() - self.start
+        playtime = round(playtime, 2)
+        return playtime
+
+
+    def destroy(self):
+        pygame.mixer.quit()
