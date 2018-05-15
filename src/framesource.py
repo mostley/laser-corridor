7e695c33e71d5c107bbcb23e81471a82906054aa
Linus Niedermaier <Linus.Niedermaier@logmein.com>
2018-05-15 15:51:12 +0200

    Add picamera to framesource

--- src/framesource.py
+++ src/framesource.py
@@ -1,26 +1,23 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import cv2
 import time
 
 class FrameSource:
     def __init__(self):
-        self.inputVideo = 0
         self.minTreshBinary = 200
-        self.capture = cv2.VideoCapture(self.inputVideo)
-
-    def getFrameDimension(self):
-        return self.capture.get(3), self.capture.get(4)
+        self.camera = picamera.PiCamera()
+        self.rawCapture = PiRGBArray(camera)
 
     def grabFrame(self):
-        while not self.capture.isOpened():
-            time.sleep(1)
+        self.camera.start_preview()
+        time.sleep(1)
         
-        check, frame = self.capture.read()
+        frame = self.camera.capture(self.rawCapture, format="bgr")
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = cv2.threshold(frame, self.minTreshBinary, 255, cv2.THRESH_BINARY)[1]
-        frame = cv2.GaussianBlur(frame, (5, 5), 0)
-
-        return check, frame
+        return frame
     
     def destroy(self):
-        self.capture.release()
+        self.camera.close()
