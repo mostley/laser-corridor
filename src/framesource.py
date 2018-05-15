43605c0a15f08bbdc0e400c72a819351937a326c
Linus Niedermaier <Linus.niederm@gmail.com>
2018-05-15 15:22:36 +0000

    Framesource without errors

--- src/framesource.py
+++ src/framesource.py
@@ -2,11 +2,13 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
 import time
+import io
 import numpy as np
 
 class FrameSource:
     def __init__(self):
 	self.minTreshBinary = 200
+	self.stream = io.BytesIO()
         #self.camera = PiCamera()
         #self.rawCapture = PiRGBArray(self.camera)
 	#self.camera.resolution = (640, 480)
@@ -17,10 +19,11 @@ class FrameSource:
 		camera.resolution = (320, 240)
 		camera.framerate = 24
 		time.sleep(1)
-		image = np.numpy((240*320*3,), dtype=np.uint8)
-		camera.capture(image, 'bgr')
+		camera.capture(self.stream, 'jpeg')
+		buff = np.fromstring(self.stream.getvalue(), dtype=np.uint8)
+		frame = cv2.imdecode(buff, 1)
 
-		frame = image.reshape((240, 320, 3))
+		#frame = image.reshape((240, 320, 3))
 		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
       	 	frame = cv2.threshold(frame, self.minTreshBinary, 255, cv2.THRESH_BINARY)[1]
 		return frame
